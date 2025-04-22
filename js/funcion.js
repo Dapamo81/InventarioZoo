9e975268a6243fffbb8051de1f233807ae90bc15
Dani <dpajaresfo@gmail.com>
2025-04-22 19:29:34 +0200

    añadimos la funcion añadir.

--- js/funcion.js
+++ js/funcion.js
@@ -3,3 +3,10 @@ function crearArray(){
     const zoo = ["León", "Elefante", "Jirafa", "Tigre", "Cebra"];
     console.log("Array inicial:", zoo);
 }
+
+function anadirArray(){
+    const zoo = ["León", "Elefante", "Jirafa", "Tigre", "Cebra"];
+    zoo.push('Ballena', 'Delfin');
+    console.log('Después de añadir dos animales:', zoo);
+
+}
