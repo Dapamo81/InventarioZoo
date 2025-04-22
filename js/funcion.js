5743fff98e37505f5f6b4ff5e24c13780498755c
Dani <dpajaresfo@gmail.com>
2025-04-22 19:34:01 +0200

    añadirmos la funcion quitar animal.

--- js/funcion.js
+++ js/funcion.js
@@ -1,12 +1,20 @@
 // Crear el array con 5 animales
 function crearArray(){
     const zoo = ["León", "Elefante", "Jirafa", "Tigre", "Cebra"];
-    console.log("Array inicial:", zoo);
+    console.log("Array inicial: ", zoo);
 }
 
 function anadirArray(){
     const zoo = ["León", "Elefante", "Jirafa", "Tigre", "Cebra"];
-    zoo.push('Ballena', 'Delfin');
-    console.log('Después de añadir dos animales:', zoo);
+    console.log("Array inicial: ", zoo);
+    zoo.push("Ballena", "Delfin");
+    console.log("Después de añadir dos animales: ", zoo);
 
+}
+
+function quitarAnimal(){
+    const zoo = ["León", "Elefante", "Jirafa", "Tigre", "Cebra"];
+    console.log("Array inicial: ", zoo);
+    zoo.pop();
+    console.log("Después de quitar un animal del final: ", zoo);
 }
