486f289ddc6427c4d991345e172e743627b392e6
Dani <dpajaresfo@gmail.com>
2025-04-22 19:37:06 +0200

    añadimos la funcion cambiar animal

--- js/funcion.js
+++ js/funcion.js
@@ -18,3 +18,10 @@ function quitarAnimal(){
     zoo.pop();
     console.log("Después de quitar un animal del final: ", zoo);
 }
+
+function cambiarAnimal(){
+    const zoo = ["León", "Elefante", "Jirafa", "Tigre", "Cebra"];
+    console.log("Array inicial: ", zoo);
+    zoo[2] = 'Tiburón';
+    console.log('Después de cambiar el tercer animal:', zoo);
+}
